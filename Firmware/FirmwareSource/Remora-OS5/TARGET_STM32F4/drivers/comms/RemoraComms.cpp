bd4cdca36b3e2838a7ac563d07623a496e48bca8
cakeslob <48161948+cakeslob@users.noreply.github.com>
2022-12-02 17:19:30 -0500

    Update RemoraComms.cpp
    https://forum.linuxcnc.org/18-computer/42276-remora-rpi-software-stepping-using-external-microcontroller-via-spi?start=570#244598
    
    I think something like this. awaiting review

--- Firmware/FirmwareSource/Remora-OS5/TARGET_STM32F4/drivers/comms/RemoraComms.cpp
+++ Firmware/FirmwareSource/Remora-OS5/TARGET_STM32F4/drivers/comms/RemoraComms.cpp
@@ -171,7 +171,20 @@ void RemoraComms::processPacket()
         // TODO: figure out how to use NORMAL mode for SPI...
         //this->status = HAL_DMA_Start(&hdma_memtomem_dma2_stream1, (uint32_t)&this->spiRxBuffer.rxBuffer, (uint32_t)this->rxData->rxBuffer, SPI_BUFF_SIZE);
         //if (this->status != HAL_OK) printf("F\n");
+         
+        // ensure an atomic access to the rxBuffer
+		// disable thread interrupts
+		__disable_irq();
 
+		// then move the data
+		for (int i = 0; i < BUFFER_SIZE; i++)
+		{
+			rxData.rxBuffer[i] = rxBuffer.rxBuffer[i];
+		}
+
+		// re-enable thread interrupts
+		__enable_irq();
+            
         // Do it the slower way. This does not seem to impact performance but not great to stay in ISR context for longer.. :-(
         for (int i = 0; i < SPI_BUFF_SIZE; i++)
         {
