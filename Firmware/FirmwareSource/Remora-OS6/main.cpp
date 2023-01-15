b626235edfeefbdc876f7de43ace59441312965a
cakeslob <48161948+cakeslob@users.noreply.github.com>
2023-01-14 22:22:26 -0500

    target manta8 to mbedos6

--- Firmware/FirmwareSource/Remora-OS6/main.cpp
+++ Firmware/FirmwareSource/Remora-OS6/main.cpp
@@ -24,9 +24,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <string> 
 #include "FATFileSystem.h"
 
-#if defined TARGET_LPC176X || TARGET_STM32F1 || TARGET_SPIDER || TARGET_MONSTER8
+#if defined TARGET_LPC176X || TARGET_STM32F1 || TARGET_SPIDER || TARGET_MONSTER8 || TARGET_MANTA8
 #include "SDBlockDevice.h"
-#elif defined TARGET_SKRV2 || TARGET_OCTOPUS || TARGET_BLACK_F407VE || TARGET_OCTOPUS_PRO_429
+#elif defined TARGET_SKRV2 || TARGET_OCTOPUS || TARGET_BLACK_F407VE || TARGET_OCTOPUS_PRO_429 
 #include "SDIOBlockDevice.h"
 #endif
 
@@ -143,6 +143,11 @@ volatile uint16_t* ptrOutputs;
 #elif defined TARGET_SPIDER
     SDBlockDevice blockDevice(PA_7, PA_6, PA_5, PA_4);  // mosi, miso, sclk, cs
     RemoraComms comms(ptrRxData, ptrTxData, SPI1, PC_6);    // use PC_6 as "slave select"
+
+
+#elif defined TARGET_MANTA8
+    SDBlockDevice blockDevice(PA_7, PA_6, PA_5, PA_4);  // mosi, miso, sclk, cs
+    RemoraComms comms(ptrRxData, ptrTxData, SPI1, PA_15);    // use PC_6 as "slave select"
 
 #endif
 
