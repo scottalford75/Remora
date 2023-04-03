abab3458f2ebc41a3cd193ece6947aeb21fda3a3
cakeslob <48161948+cakeslob@users.noreply.github.com>
2023-04-03 00:41:30 -0400

    Update qeiDriver.cpp
    encoder fix from flexi-hal
    encoder stops counting after 15.9999
    this appears to fix the issue

--- Firmware/FirmwareSource/Remora-OS5/TARGET_STM32F4/drivers/qei/qeiDriver.cpp
+++ Firmware/FirmwareSource/Remora-OS5/TARGET_STM32F4/drivers/qei/qeiDriver.cpp
@@ -47,7 +47,8 @@ void QEIdriver::init()
     this->htim.Instance = TIM1;
     this->htim.Init.Prescaler = 0;
     this->htim.Init.CounterMode = TIM_COUNTERMODE_UP;
-    this->htim.Init.Period = 65535;
+    this->htim.Init.Period = 0xffffffff; // 32-bit count
+    //this->htim.Init.Period = 65535;
     this->htim.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
     this->htim.Init.RepetitionCounter = 0;
 
